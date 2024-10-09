8f80d3a1b7040d12186de881149dbbe302caa200
srikar <srikarkanakala666@gmail.com>
2024-10-09 20:43:08 +0530

    version-L

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ const upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number= "0123456789";
 const specialSymbols="!@#$%^&*()+<>:;-=`~";
+const allchars=upperCase+lowerCase+number+specialSymbols
 function createPassword(){
   let password ="";
-  password +=upperCase
+  password +=upperCase[Math.floor(Math.random()*upperCase.length)];
+  password +=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+  password +=number[Math.floor(Math.random()*number.length)];
+  password +=specialSymbols[Math.floor(Math.random()*specialSymbols.length)];
+
+  while(length>password.length){
+    password +=allchars[Math.floor(Math.random()*allchars.length)];
+  }
+  passwordBox.value=password
+
+}
+function passwordCopy(){
+  passwordBox.select();
+  document.execCommand("Copy");
+
 }
